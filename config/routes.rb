052fad7439b5373e1c97d2bc6392b994fd75a327
Thomas Dorn <dorn.thomas@gmail.com>
2012-12-17 15:05:32 -0800

    dbaster

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Altruus2::Application.routes.draw do
+  get "sessions/new"
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "static_pages#home"
   resources :users
   resources :sessions
- 
-
-  resources :adds
+ resources :adds
 
 
   get "static_pages/home"
   get "static_pages/help"
+
 end

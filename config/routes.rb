49bb511b87c2294a1fc257340c445883c18f6340
Thomas Dorn <dorn.thomas@gmail.com>
2012-12-17 14:28:29 -0800

    started work on user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Altruus2::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
-  get "sign_up" => "usermodel#new", :as => "sign_up"
-  root :to => "usermodel#new"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
   resources :users
   resources :sessions
-
-  resources :users
  
 
   resources :adds
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217214911) do
+ActiveRecord::Schema.define(:version => 20121217221551) do
 
   create_table "adds", :force => true do |t|
     t.string   "advertistment"
@@ -29,10 +29,11 @@ ActiveRecord::Schema.define(:version => 20121217214911) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
